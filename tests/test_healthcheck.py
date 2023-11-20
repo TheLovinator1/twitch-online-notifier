f7494704dda187c2991d36c9cda97b1ad3ebed93
Joakim Hellsén <tlovinator@gmail.com>
2023-11-20 22:22:00 +0100

    Remove class from test

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -1,5 +1,3 @@
-import unittest
-from typing import Self
 from unittest.mock import MagicMock, patch
 
 import requests
@@ -7,45 +5,37 @@ import requests
 from twitch_online_notifier.healthcheck import healthcheck
 
 
-class TestHealthcheck(unittest.TestCase):
-    """Test the healthcheck function.
+@patch("requests.get")
+@patch("sys.exit")
+def test_healthcheck_success(
+    mock_sys_exit: MagicMock,
+    mock_requests_get: MagicMock,
+) -> None:
+    """Fake a successful response from the website."""
+    mock_response = MagicMock()
+    mock_response.ok = True
+    mock_requests_get.return_value = mock_response
 
-    Args:
-        unittest: The unittest class.
-    """
+    # Run the healthcheck function.
+    healthcheck()
 
-    @patch("requests.get")
-    @patch("sys.exit")
-    def test_healthcheck_success(
-        self: Self,  # noqa: PLR6301
-        mock_sys_exit: MagicMock,
-        mock_requests_get: MagicMock,
-    ) -> None:
-        """Fake a successful response from the website."""
-        mock_response = MagicMock()
-        mock_response.ok = True
-        mock_requests_get.return_value = mock_response
+    # Check that sys.exit was called with 0. (Success)
+    mock_sys_exit.assert_called_with(0)
 
-        # Run the healthcheck function.
-        healthcheck()
 
-        # Check that sys.exit was called with 0. (Success)
-        mock_sys_exit.assert_called_with(0)
+@patch("requests.get")
+@patch("sys.exit")
+def test_healthcheck_failure(
+    mock_sys_exit: MagicMock,
+    mock_requests_get: MagicMock,
+) -> None:
+    """Fake a failed response from the website."""
+    mock_requests_get.side_effect = requests.exceptions.RequestException(
+        "Connection error",
+    )
 
-    @patch("requests.get")
-    @patch("sys.exit")
-    def test_healthcheck_failure(
-        self: Self,  # noqa: PLR6301
-        mock_sys_exit: MagicMock,
-        mock_requests_get: MagicMock,
-    ) -> None:
-        """Fake a failed response from the website."""
-        mock_requests_get.side_effect = requests.exceptions.RequestException(
-            "Connection error",
-        )
+    # Run the healthcheck function.
+    healthcheck()
 
-        # Run the healthcheck function.
-        healthcheck()
-
-        # Check that sys.exit was called with 1. (Failure)
-        mock_sys_exit.assert_called_with(1)
+    # Check that sys.exit was called with 1. (Failure)
+    mock_sys_exit.assert_called_with(1)
