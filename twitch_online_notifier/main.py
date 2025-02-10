808fbaf2ff62cb16440dc5659599041572406647
Joakim Hellsén <tlovinator@gmail.com>
2025-02-10 02:28:33 +0100

    Remove loguru and add support for uv

--- twitch_online_notifier/main.py
+++ twitch_online_notifier/main.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 import asyncio
+import logging
 import os
 import sys
 from typing import TYPE_CHECKING
 
+import sentry_sdk
 from discord_webhook import DiscordWebhook
-from dotenv import find_dotenv, load_dotenv
-from loguru import logger
+from dotenv import load_dotenv
 from twitchAPI.eventsub.webhook import EventSubWebhook
 from twitchAPI.twitch import Twitch
 from twitchAPI.type import (
@@ -22,7 +23,17 @@ if TYPE_CHECKING:
     from requests import Response
     from twitchAPI.object.eventsub import StreamOnlineEvent
 
-load_dotenv(find_dotenv(), verbose=True)
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+sentry_sdk.init(
+    dsn="https://d86618c233104cf6f82cffc93db087cf@o4505228040339456.ingest.us.sentry.io/4508792266358784",
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+    _experiments={"continuous_profiling_auto_start": True},
+)
+
+load_dotenv(verbose=True)
 
 app_id: str = os.getenv("TWITCH_APP_ID", "")
 app_secret: str = os.getenv("TWITCH_APP_SECRET", "")
@@ -31,24 +42,16 @@ eventsub_url: str = os.getenv("EVENTSUB_URL", "")
 webhook_url: str = os.getenv("WEBHOOK_URL", "")
 error_webhook_url: str = os.getenv("ERROR_WEBHOOK_URL", "")
 
-if (
-    app_id == ""
-    or app_secret == ""
-    or twitch_usernames == ""
-    or eventsub_url == ""
-    or webhook_url == ""
-):
+if not app_id or not app_secret or not twitch_usernames or not eventsub_url or not webhook_url:
     logger.error("Missing environment variables. Please check your .env file.")
     sys.exit(1)
 
-usernames = []
-if twitch_usernames is not None:
-    usernames: list[str] = twitch_usernames.split(",")
+usernames: list[str] = twitch_usernames.split(",")
 
 logger.info("Starting twitch-online-notifier...")
 logger.info("Usernames to listen for:")
 for username in usernames:
-    logger.info(f"\t- {username}")
+    logger.info("\t- %s", username)
 
 
 def send_message_to_discord(message: str, *, if_error: bool = False) -> None:
@@ -58,23 +61,22 @@ def send_message_to_discord(message: str, *, if_error: bool = False) -> None:
         message: The message to send.
         if_error: If the message is an error message.
     """
-    webhook_to_use = webhook_url
+    webhook_to_use: str = webhook_url
     if if_error:
         logger.error(message)
-        webhook_to_use: str = (
-            error_webhook_url if error_webhook_url != "" else webhook_url
-        )
+        webhook_to_use = error_webhook_url or webhook_url
 
-    webhook: DiscordWebhook = DiscordWebhook(
-        url=webhook_to_use,
-        content=message,
-        rate_limit_retry=True,
-    )
+    webhook: DiscordWebhook = DiscordWebhook(url=webhook_to_use, content=message, rate_limit_retry=True)
     response: Response = webhook.execute()
 
     if not response.ok:
+        logger.error(
+            "Webhook failed when sending last message.\nStatus code: '%s'\nMessage: '%s'",
+            response.status_code,
+            message,
+        )
         send_message_to_discord(
-            f"Webhook failed when sending last message.\nStatus code: '{response.status_code}'\nMessage: '{message}'",  # noqa: E501
+            f"Webhook failed when sending last message.\nStatus code: '{response.status_code}'\nMessage: '{message}'",
             if_error=True,
         )
 
@@ -89,8 +91,8 @@ async def on_live(data: StreamOnlineEvent) -> None:  # noqa: RUF029
     broadcaster_login: str = data.event.broadcaster_user_login
     broadcaster_url: str = f"https://twitch.tv/{broadcaster_login}"
 
-    logger.info(f"{broadcaster_user_name} is live!")
-    logger.info(f"\tURL: {broadcaster_url}")
+    logger.info("%s is live!", broadcaster_user_name)
+    logger.info("\t%s", broadcaster_url)
 
     send_message_to_discord(f"{broadcaster_user_name} is live!\n{broadcaster_url}")
 
@@ -109,7 +111,7 @@ def send_twitch_error_message(exception: Exception) -> None:
     elif isinstance(exception, EventSubSubscriptionError):
         msg = "The subscription request was invalid."
     elif isinstance(exception, TwitchBackendException):
-        msg = "I think the Twitch API is down? We tried to subscribe to a user but it failed."  # noqa: E501
+        msg = "I think the Twitch API is down? We tried to subscribe to a user but it failed."
     send_message_to_discord("twitch-online-notifier - ERROR: " + msg, if_error=True)
 
 
@@ -126,7 +128,7 @@ async def main() -> None:
 
     try:
         async for user in twitch.get_users(logins=usernames):
-            logger.info(f"Listening for events for '{user.login}'.")
+            logger.info("Listening for events for '%s' (%s)", user.display_name, user.id)
             try:
                 await eventsub.listen_stream_online(
                     broadcaster_user_id=user.id,
@@ -139,7 +141,7 @@ async def main() -> None:
     except TwitchAPIException as e:
         send_twitch_error_message(e)
 
-    logger.info(f"I am now listening for events on {eventsub_url} :-)")
+    logger.info("I am now listening for events on %s", eventsub_url)
 
 
 def start() -> None:
